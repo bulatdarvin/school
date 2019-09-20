6e7c8e29e780409544e541c7053ca06f22afeb78
Seeium Silvana <ssilvana@oa-h6.21-school.ru>
2019-09-20 18:37:41 +0300

    sfa

--- fillit/read.c
+++ fillit/read.c
@@ -13,6 +13,24 @@
 #include "header.h"
 #include <stdio.h>
 
+void		ft_lstrev(t_tet **alst)
+{
+	t_tet	*prev;
+	t_tet	*cur;
+	t_tet	*next;
+
+	prev = NULL;
+	cur = *alst;
+	while (cur != NULL)
+	{
+		next = cur->next;
+		cur->next = prev;
+		prev = cur;
+		cur = next;
+	}
+	*alst = prev;
+}
+
 char	*readstr(int fd)
 {
 	int		ret;
@@ -86,49 +104,54 @@ t_tet	*ft_create_elem(int *data)
 {
 	t_tet	*tmp;
 
-	//tmp = malloc(50);
 	tmp = (t_tet*)malloc(sizeof(t_tet));
 	if (tmp)
 	{
-		tmp->tet_id = ft_memalloc(sizeof(data));
-		if (tmp->tet_id == 0)
-			return (NULL);
+	//	tmp->tet_id = ft_memalloc(sizeof(data));
+	//	if (tmp->tet_id == 0)
+	//		return (NULL);
 		tmp->tet_id = data;
 		tmp->next = NULL;
 		return (tmp);
 	}
 	else
-	{
-		free(tmp);
 		return (NULL);
-	}
 }
 
 void	ft_push_back(t_tet **begin_list, int *data)
 {
 	t_tet	*list;
+	static char i = 'a';
+	int k = 0;
 
-	list = *begin_list;
-	if (list)
+	if (*begin_list)
 	{
-		ft_putchar('a');
+		list = *begin_list;
 		while (list->next)
+		{
+			ft_putnbr(k);
 			list = list->next;
-		ft_putchar('a');
+			k++;
+		}
 		list->next = ft_create_elem(data);
+		list->c = i;
 	}
 	else
+	{
 		*begin_list = ft_create_elem(data);
-
+		(*begin_list)->c = i;
+	}
+	i++;
 }
 
-void	init(char *line, int k, t_tet **elem)
+t_tet	*init(char *line, int k, t_tet *elem)
 {
 	int i;
 	int x;
 	int y;
 	int t;
 	int *a;
+	t_tet *head;
 
 	i = 0;
 	x = 0;
@@ -147,6 +170,10 @@ void	init(char *line, int k, t_tet **elem)
 				{
 					a[t++] = x;
 					a[t++] = y;
+				//	ft_putnbr(a[t - 2]);
+				//	ft_putchar(' ');
+				//	ft_putnbr(a[t - 1]);
+				//	ft_putchar('\n');
 				}
 				x++;
 				i++;
@@ -155,18 +182,85 @@ void	init(char *line, int k, t_tet **elem)
 			y++;
 			i++;
 		}
-		ft_putnbr(i);
-		ft_push_back(elem, a);		
+	//	ft_putchar('\n');
+		ft_push_back(&elem, a);
+		//if (i == 20)
+		//	head = elem;
+		//else
+		//	head->next = elem;
 		y = 0;
+		t = 0;
 		i++;
 	}
+	free(a);
+	ft_lstrev(&elem);
+	return (elem);
 }
 
 int		warn()
 {
 	ft_putstr("error");
-	return (1);
+	return (0);
 }
+
+
+int		*shift(int *a)
+{
+	int i;
+	int y;
+	int x;
+
+	i = 3;
+	y = a[1];
+	x = a[0];
+	while (i < 8)
+	{
+		if (y > a[i] && i % 2 == 1)
+			y = a[i];
+		if (x > a[i] && i % 2 == 0)
+			x = a[i];
+		i = i + 2;
+	}
+	i = 0;
+	while (i < 8)
+	{
+		if (i % 2 == 0)
+			a[i] = a[i] - x;
+		else
+			a[i] = a[i] - y;
+		i = i + 2;
+	}
+	return (a);
+}
+
+void		checkpatterns(t_tet **begin)
+{
+	t_tet	*tmp;
+	t_tet	*elem;
+	int		x;
+	int		y;
+	int i = 0;
+
+	tmp = *begin;
+/*	while (tmp)
+	{
+		tmp->tet_id = shift(tmp->tet_id);
+		tmp = tmp->next;
+	}
+*/	elem = *begin;
+	while (elem)
+	{
+		while (i < 8)
+		{
+			printf("%d\n", elem->tet_id[i]);
+			i++;
+		}
+		printf("\n");
+		i = 0;
+		elem = elem->next;
+	}
+}
+
 int		main(int argc, char **argv)
 {
 	char	*line;
@@ -174,6 +268,7 @@ int		main(int argc, char **argv)
 	int		k;
 	int		*a;
 	t_tet	*elem;
+	t_tet	*head;
 
 	if (argc != 2)
 		return (0);	//razobratsia chto takoe usage
@@ -187,20 +282,18 @@ int		main(int argc, char **argv)
 		return (0);
 	}
 	k = kolvo(line);
-
-	init(line, k, &elem);
-	ft_putchar('b');
+	head = init(line, k, elem);
 	int i = 0;
-	int j = 0;
-	while (elem)
+	while (head)
 	{
 		while (i < 8)
 		{
-			printf("%d\n", elem->tet_id[i]);
+			printf("%d\n", head->tet_id[i]);
 			i++;
 		}
-		elem = elem->next;
+		printf("         %c\n\n", head->c);
+		i = 0;
+		head = head->next;
 	}
-
 	return (0);
 }
